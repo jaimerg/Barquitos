02b4b8f9a13f9a82908f58f0ff5369f8f223238d
jaime <jaimerg21@gmail.com>
2015-03-17 18:47:39 +0100

    Prueba de subida 3

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var divs = '';
 var barco = '';
 
 
+
 //funcion que te da la letra siguiente
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
@@ -14,11 +15,10 @@ function nextChar(c) {
 function tablero() {
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
-            divs = divs + "<div onmouseover='checkPositionBarcu(this)' id='" + letra + j + "' class='divTablero' "
-                    +"ondrop='drop(event)' ondragover='allowDrop(event)'><!-- -" +
-                    letra + (j + 1) + "- --><img src='imagenes/mar.png' width='50px' height='50px'></div>";
+            divs = divs + "<div onmouseover='checkPositionBarcu(this)' id='t" + i +"t"+ j + "' ocupado='no' class='divTablero' "
+                    +"ondrop='drop(event)' ondragover='allowDrop(event)'></div>";
         }
-        letra = nextChar(letra);
+        //letra = nextChar(letra);
         divs = divs + "<br />";
     }
     // Se mostrara el tablero de juego
@@ -32,8 +32,8 @@ function checkPositionBarcu(elem){
 function barcos() {
     barco = barco + "<table border='1'>";
     barco = barco + "<tr>";
-    barco = barco + "<td><div id='qwe' aling='center' draggable='true' ondragstart='drag(event)' > <img src='imagenes/barco1.png' width='50px' height='50px'> </div></td>";
-    barco = barco + "<td><div id='qwe1' long='2' ori='h' draggable='true' ondragstart='drag(event)' onclick='orientacion(this);'> <img src='imagenes/barcobarcoh.png' id='ass' width='100px' height='50px'> </div></td>";
+    barco = barco + "<td><div  > <img id='barco1' long='1' ori='h' draggable='true' ondragstart='drag(event)' src='imagenes/barco1.png' width='50px' height='50px'> </div></td>";
+    barco = barco + "<td><div > <img id='barco2' long='2' ori='h' draggable='true' ondragstart='drag(event)' onclick='orientacion(this);' src='imagenes/barcobarcoh.png' id='ass' width='100px' height='50px'> </div></td>";
     barco = barco + "<td><div id='papelera'><img src='imagenes/papelera.png' width='100px' heigth='50px'></td>";
     barco = barco + '<td><div id="papelera" ondrop="return eliminar(event)" ondragleave="return leave(event)" ondragover="return over(event)" ondragenter="return enter(event)">Papelera</div>';
     barco = barco + "</tr>";
@@ -54,42 +54,80 @@ function orientacion(asd){
         document.getElementById('qwe1').innerHTML = h;
         asd.setAttribute('ori','h');
     }
-    
  }
 
 //funciones para drag
-
 function allowDrop(ev) {
     ev.preventDefault();
+    /*
+    var id = ev.dataTransfer.getData("text");
+    var barco = document.getElementById(id);
+    
+    var long = barco.getAttribute('long');
+    var ori = barco.getAttribute('ori');
+    
+    console.log("id"+long+":"+"ori");*/
 }
 var idBarcos = 0;
 var orientacionBarco;
 var longitudBarco;
+
 function drag(ev) {
-    var imagen = ev.target.src;
+    var imagen = ev.target.id;
     ev.dataTransfer.setData("text", imagen);
     
 }
 
 function drop(ev) {
     ev.preventDefault();
+    var id = ev.dataTransfer.getData("text");
+    var barco = document.getElementById(id);
     
-    var elementoArrastrado = document.getElementById(ev.dataTransfer.getData("Data")); // Elemento arrastrado
-            elementoArrastrado.parentNode.removeChild(elementoArrastrado); // Elimina el elemento
+    var long = barco.getAttribute('long');
+    var ori = barco.getAttribute('ori');
+    var objetivo = ev.target;
+    var idObjetivo = objetivo.id;
+    var res = idObjetivo.split("t");
+    //ev.target.src =;
+   // ev.target.id = "barquito" + idBarcos;
+   
+    console.log("id: "+id+" long: "+long+" ori: "+ori+" obj:"+idObjetivo);
+    console.log(res[1] +":"+ res[2]);
+    var fila = parseInt(res[1]);
+    var columna = parseInt(res[2]);
     
-    
-    /*
-    var elementoArrastrado = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(elementoArrastrado));*/
-    
-    if(elementoArrastrado=='pepito'){
-        //situar dos imatges en les posicions indicades
+    var ocupado = objetivo.getAttribute('ocupado');
+    if(ocupado == "no"){
+        
+        if(barco.getAttribute('long')==1){
+            objetivo.style.backgroundImage = "url('"+barco.src+"')";
+            objetivo.setAttribute('ocupado',"si");
+        }
+        else if(barco.getAttribute('long')==2){
+            if(barco.getAttribute('ori')=="h"){
+                
+                var id2 = "t"+fila+"t"+(columna+1); 
+                var objetivo2 = document.getElementById(id2);
+                var ocupado2 = objetivo2.getAttribute('ocupado')
+                if(ocupado2 == "no"){
+                    objetivo.style.backgroundImage = "url(imagenes/b1.png)";
+                    objetivo2.style.backgroundImage = "url(imagenes/b2.png)";
+                    objetivo.setAttribute('ocupado',"si");
+                    objetivo2.setAttribute('ocupado',"si");
+                }
+                
+            }
+            if(barco.getAttribute('ori')=="v"){
+                
+            }
+           
+        }
+        
+        
         
     }
-
-    ev.target.src = imagen;
-    ev.target.id = "barquito" + idBarcos;
-    idBarcos++;
+    
+    //idBarcos++;
 }
 /*
 function eliminar(ev){
